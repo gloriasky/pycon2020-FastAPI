d6a2dabc7ca71d6c022b69860e55aed7b75cb080
Valadzko Kseniya <volodko.kj@gmail.com>
2020-02-21 11:10:26 +0300

    task 1

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class UserCreate(BaseModel):
+    email: str
+    password: str
 
 @app.get("/")
 def read_root():
@@ -11,3 +15,8 @@ def read_root():
 @app.get("/users/{user_id}")
 def read_item(user_id: int):
     return {"item_id": user_id, "email": "john.doe@example.com"}
+
+
+@app.post("/users")
+def create_user(user: UserCreate):
+    return user
