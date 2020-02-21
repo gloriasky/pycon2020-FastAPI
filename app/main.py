388a08ac38021e9b11772c507b1cbb461699f6b1
Valadzko Kseniya <volodko.kj@gmail.com>
2020-02-21 12:35:55 +0300

    smth

--- app/main.py
+++ app/main.py
@@ -1,22 +1,51 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from typing import List
+
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+import sys
+sys.path.append(".\\")
+import crud, models, schemas
+from database import SessionLocal, engine
+from schemas import User, UserCreate
+
 
 app = FastAPI()
 
-class UserCreate(BaseModel):
-    email: str
-    password: str
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/users/{user_id}")
-def read_item(user_id: int):
-    return {"item_id": user_id, "email": "john.doe@example.com"}
+@app.get("/users/", response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
 
 
-@app.post("/users")
-def create_user(user: UserCreate):
-    return user
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+
+@app.post("/users", response_model=User)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user =  crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already exists")
+    return crud.create_user(db=db, user = user)
+
+
+@app.on_event("startup")
+def startup_event():
+    models.Base.metadata.create_all(bind=engine)
